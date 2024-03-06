083274ca30df998b8eb118ca58729c1421a4f2d7
Sandeep Dash <contactsandeepdash@gmail.com>
2024-03-06 11:15:01 +0530

    logging

--- src/main/java/com/dash/controller/HelloWorldController.java
+++ src/main/java/com/dash/controller/HelloWorldController.java
@@ -5,15 +5,20 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import jakarta.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller("/")
 public class HelloWorldController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 
     @Inject
     HelloWorldService helloWorldService;
 
     @Get(produces = MediaType.TEXT_PLAIN)
     public String welcome() {
+        logger.debug("Called the Welcome API!");
         return helloWorldService.welcomeFromService();
     }
 
